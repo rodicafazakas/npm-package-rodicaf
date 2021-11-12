7c8f313938963f411ec366edcad671970c4bff81
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-12 13:27:14 +0100

    Changed function

--- index.js
+++ index.js
@@ -4,21 +4,14 @@ const miPaquete = require("exercise-clean-folder-rodicaf");
 const fs = require("fs");
 
 
-fs.readdir("./DeleteMe", async (error,files)=>{
+fs.readdir("./DeleteMe", async (error, files)=>{
     if(error) throw error;
-    console.log('aaaaaaaaaaaaaaaaaa');
 
-    const filesToDelete = {
-        file1: 'npm-package_1.docx',
-        file2: 'npm-package_2.docx',
-        file3: 'npm-package_3.docx'
-    }
-
-    for(let key in filesToDelete) {
-        console.log(filesToDelete[key])
-        await fs.unlink('./DeleteMe/' + filesToDelete[key], (error) => { 
+    for(let key in files) {
+        // console.log(filesToDelete[key])
+        await fs.unlink('./DeleteMe/' + files[key], (error) => { 
         if (error) throw error;
-        console.log('File deleted: ' + filesToDelete[key]);
+        console.log('File deleted: ' + files[key]);
         })
     }
 });   
